a642e4a37c10f64a02ca2da94e99516bec364e67
Victor Garcia-Vao <[victorgarciavao@gmail.com]>
2022-06-27 18:05:56 +0200

    first structure added

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import logoVictor from './images/logo-victor.png';
+
+function App() {
+  return (
+    <div className='App'>
+      <div className="container-logo">
+        <img 
+          src={logoVictor} 
+          alt="Logo victor" 
+          className="logo" />
+      </div>
+
+      <div className="main-container">
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
